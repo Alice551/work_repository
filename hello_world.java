15c4354752e596a6d61e3a39c084c23fa1f4cc24
Alice551 <assgxgj@gmail.com>
2018-07-29 01:00:59 +0900

    Test commit3

--- hello_world.java
+++ hello_world.java
@@ -4,6 +4,9 @@ public class hello_world {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
 		System.out.println("Test");
+		for ( int index = 0; index < 5; ++index ) {
+			System.out.println(index);
+		}
 	}
 
 }
